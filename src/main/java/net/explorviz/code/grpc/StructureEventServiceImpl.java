3bf391372aa0750627c569298170826b499ee420
Alexander Krause <akr@informatik.uni-kiel.de>
2022-01-10 12:21:16 +0100

    renaming grpc service

---
+++ src/main/java/net/explorviz/code/grpc/StructureEventServiceImpl.java
@@ -0,0 +1,38 @@
+package net.explorviz.code.grpc;
+
+import com.google.protobuf.Empty;
+import io.quarkus.grpc.GrpcService;
+import io.smallrye.mutiny.Uni;
+import net.explorviz.code.proto.StructureCreateEvent;
+import net.explorviz.code.proto.StructureDeleteEvent;
+import net.explorviz.code.proto.StructureEventService;
+import net.explorviz.code.proto.StructureModifyEvent;
+
+@GrpcService
+public class StructureEventServiceImpl implements StructureEventService {
+
+  // grpcurl -plaintext localhost:9000 list
+  // grpcurl -plaintext -d '{"name": "test"}' localhost:9000 helloworld.Greeter.SayHello
+  // grpcurl -plaintext localhost:9000 describe net.explorviz.code.proto.StructureEventService
+  // grpcurl -plaintext -d '{"className": "test"}' localhost:9000
+  // net.explorviz.code.proto.StructureEventService.sendCreateEvent
+
+  @Override
+  public Uni<Empty> sendCreateEvent(final StructureCreateEvent request) {
+    System.out.println("Client says: " + request.getClassName());
+    return Uni.createFrom().item(() -> Empty.newBuilder().build());
+  }
+
+  @Override
+  public Uni<Empty> sendDeleteEvent(final StructureDeleteEvent request) {
+    System.out.println("Client says: " + request.getClassName());
+    return Uni.createFrom().item(() -> Empty.newBuilder().build());
+  }
+
+  @Override
+  public Uni<Empty> sendModifyEvent(final StructureModifyEvent request) {
+    System.out.println("Client says: " + request.getClassName());
+    return Uni.createFrom().item(() -> Empty.newBuilder().build());
+  }
+
+}
