a09255d3e948d99a5c37a9cbcf71531a2c2d69c8
Alexander Krause <akr@informatik.uni-kiel.de>
2022-11-03 14:35:53 +0100

    dependencies updated and wip gRPC receive

--- src/main/java/net/explorviz/code/grpc/StructureEventServiceImpl.java
+++ src/main/java/net/explorviz/code/grpc/StructureEventServiceImpl.java
@@ -3,16 +3,18 @@ package net.explorviz.code.grpc;
 import com.google.protobuf.Empty;
 import io.quarkus.grpc.GrpcService;
 import io.smallrye.mutiny.Uni;
-import net.explorviz.code.proto.StructureCreateEvent;
-import net.explorviz.code.proto.StructureDeleteEvent;
 import net.explorviz.code.proto.StructureEventService;
-import net.explorviz.code.proto.StructureModifyEvent;
+import net.explorviz.code.proto.StructureFileEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * GrpcService that listens to StructureEvents. Entrypoint for this service.
  */
 @GrpcService
 public class StructureEventServiceImpl implements StructureEventService {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(StructureEventServiceImpl.class);
 
   // grpcurl -plaintext localhost:9000 list
   // grpcurl -plaintext -d '{"name": "test"}' localhost:9000 helloworld.Greeter.SayHello
@@ -21,18 +23,8 @@ public class StructureEventServiceImpl implements StructureEventService {
   // net.explorviz.code.proto.StructureEventService.sendCreateEvent
 
   @Override
-  public Uni<Empty> sendCreateEvent(final StructureCreateEvent request) {
+  public Uni<Empty> sendStructureFileEvent(StructureFileEvent request) {
+    LOGGER.trace("Received message: {}", request);
     return Uni.createFrom().item(() -> Empty.newBuilder().build());
   }
-
-  @Override
-  public Uni<Empty> sendDeleteEvent(final StructureDeleteEvent request) {
-    return Uni.createFrom().item(() -> Empty.newBuilder().build());
-  }
-
-  @Override
-  public Uni<Empty> sendModifyEvent(final StructureModifyEvent request) {
-    return Uni.createFrom().item(() -> Empty.newBuilder().build());
-  }
-
 }
