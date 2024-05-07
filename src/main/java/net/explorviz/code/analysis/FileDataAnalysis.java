8cc6d7ad1c93770f921897b208dbfb47006154b1
Alexander Krause <akr@informatik.uni-kiel.de>
2024-05-07 09:47:28 +0200

    refactor step 1 for filedatanaylsis

--- src/main/java/net/explorviz/code/analysis/FileDataAnalysis.java
+++ src/main/java/net/explorviz/code/analysis/FileDataAnalysis.java
@@ -33,196 +33,205 @@ public class FileDataAnalysis {
    *
    * @param fileData the FileData to handle
    */
-  public void processFileData(final FileData fileData) { // NOPMD
-
+  public void processFileData(final FileData fileData) {
     LOGGER.atTrace().addArgument(fileData).log("Received file data: {}");
 
+    FileReportTable fileReportTable = updateOrCreateFileReportTable(fileData);
+    FileReport fileReport = createFileReport(fileData, fileReportTable);
+    updateFileReport(fileData, fileReport);
+  }
+
+  private FileReportTable updateOrCreateFileReportTable(FileData fileData) {
     final String receivedFileDataLandscapeToken = fileData.getLandscapeToken();
     final String receivedFileDataAppName = fileData.getApplicationName();
     final String receivedFileDataCommitId = fileData.getCommitID();
-    final String receivedFileDataFileName = fileData.getFileName();
     final String receivedFileDataPackageName = fileData.getPackageName();
-    final List<String> receivedFileDataImportName = fileData.getImportNameList();
-    final Map<String, ClassData> receivedFileDataClassData = fileData.getClassDataMap();
-    final Map<String, String> receivedFileDataMetric = fileData.getMetricMap();
-    final String receivedFileDataAuthor = fileData.getAuthor();
-    final String receivedFileDataModifiedLines = fileData.getModifiedLines();
-    final String receivedFileDataAddedLines = fileData.getAddedLines();
-    final String receivedFileDataDeletedLines = fileData.getDeletedLines();
-
-    final FileReport fileReport = new FileReport();
-
-    // FileReport Table ------------------------------------------------------------------
-    final FileReportTable fileReportTable = FileReportTable
-        .findByTokenAndAppName(receivedFileDataLandscapeToken, receivedFileDataAppName);
+    final String receivedFileDataFileName = fileData.getFileName();
     final String fqFileName = receivedFileDataPackageName + "." + receivedFileDataFileName;
+
+    FileReportTable fileReportTable = FileReportTable
+        .findByTokenAndAppName(receivedFileDataLandscapeToken, receivedFileDataAppName);
 
     if (fileReportTable == null) {
       LOGGER.atTrace().log("CREATE FILE REPORT TABLE.");
-      final FileReportTable newFileReportTable = new FileReportTable();
-      newFileReportTable.setLandscapeToken(receivedFileDataLandscapeToken);
-      newFileReportTable.setAppName(receivedFileDataAppName);
-      final Map<String, Map<String, String>> table = new HashMap<>();
-      final Map<String, String> fqFileNameToCommitId = new HashMap<>();
-      fqFileNameToCommitId.put(fqFileName, receivedFileDataCommitId);
-      // after the table is filled we can store it
-      table.put(receivedFileDataCommitId, fqFileNameToCommitId);
-      newFileReportTable.setCommitIdTofqnFileNameToCommitIdMap(table);
-      newFileReportTable.persist();
+      fileReportTable =
+          createNewFileReportTable(receivedFileDataLandscapeToken, receivedFileDataAppName,
+              receivedFileDataCommitId, fqFileName);
     } else {
-      // File Report Table already exists. We only add entries by manipulating the map.
-
-      final Map<String, Map<String, String>> table = fileReportTable
-          .getCommitIdTofqnFileNameToCommitIdMap();
-
-      final boolean keyExists = table.containsKey(receivedFileDataCommitId);
-
-      Map<String, String> fqFileNameToCommitIdMap = new HashMap<>();
-      if (!keyExists) {
-        LOGGER.warn("Normally Commit Report should be received before one of its File Reports");
-      } else {
-        fqFileNameToCommitIdMap = table.get(receivedFileDataCommitId);
-      }
-      // fill missing entry for current file report (might overwrite entry copied from parent)
-      fqFileNameToCommitIdMap.put(fqFileName, receivedFileDataCommitId);
-      table.put(receivedFileDataCommitId, fqFileNameToCommitIdMap);
-      fileReportTable.setCommitIdTofqnFileNameToCommitIdMap(table);
-      fileReportTable.update();
+      updateExistingFileReportTable(fileReportTable, receivedFileDataCommitId, fqFileName);
     }
+    return fileReportTable;
+  }
 
-    // -----------------------------------------------------------------------------------------
+  private FileReportTable createNewFileReportTable(String token, String appName, String commitId,
+      String fqFileName) {
+    final FileReportTable newFileReportTable = new FileReportTable();
+    newFileReportTable.setLandscapeToken(token);
+    newFileReportTable.setAppName(appName);
+    final Map<String, Map<String, String>> table = new HashMap<>();
+    final Map<String, String> fqFileNameToCommitId = new HashMap<>();
+    fqFileNameToCommitId.put(fqFileName, commitId);
+    table.put(commitId, fqFileNameToCommitId);
+    newFileReportTable.setCommitIdTofqnFileNameToCommitIdMap(table);
+    newFileReportTable.persist();
+    return newFileReportTable;
+  }
 
-    fileReport.setLandscapeToken(receivedFileDataLandscapeToken);
-    fileReport.setAppName(receivedFileDataAppName);
-    fileReport.setCommitId(receivedFileDataCommitId);
-    fileReport.setFileName(receivedFileDataFileName);
-    fileReport.setPackageName(receivedFileDataPackageName);
-    fileReport.setImportName(receivedFileDataImportName);
+  private void updateExistingFileReportTable(FileReportTable fileReportTable, String commitId,
+      String fqFileName) {
+    final Map<String, Map<String, String>> table =
+        fileReportTable.getCommitIdTofqnFileNameToCommitIdMap();
+    Map<String, String> fqFileNameToCommitIdMap = table.getOrDefault(commitId, new HashMap<>());
+    fqFileNameToCommitIdMap.put(fqFileName, commitId);
+    table.put(commitId, fqFileNameToCommitIdMap);
+    fileReportTable.setCommitIdTofqnFileNameToCommitIdMap(table);
+    fileReportTable.update();
+  }
 
+  private FileReport createFileReport(FileData fileData, FileReportTable fileReportTable) {
+    FileReport fileReport = new FileReport();
+    fillFileReportBasicInfo(fileReport, fileData);
+    fillClassData(fileReport, fileData.getClassDataMap());
+    return fileReport;
+  }
+
+  private void fillFileReportBasicInfo(FileReport fileReport, FileData fileData) {
+    fileReport.setLandscapeToken(fileData.getLandscapeToken());
+    fileReport.setAppName(fileData.getApplicationName());
+    fileReport.setCommitId(fileData.getCommitID());
+    fileReport.setFileName(fileData.getFileName());
+    fileReport.setPackageName(fileData.getPackageName());
+    fileReport.setImportName(fileData.getImportNameList());
+    fileReport.setFileMetric(fileData.getMetricMap());
+    fileReport.setAuthor(fileData.getAuthor());
+    fileReport.setModifiedLines(fileData.getModifiedLines());
+    fileReport.setAddedLines(fileData.getAddedLines());
+    fileReport.setDeletedLines(fileData.getDeletedLines());
+  }
+
+  private void fillClassData(FileReport fileReport,
+      Map<String, ClassData> receivedFileDataClassData) {
     final Map<String, ClassData2> classData = new HashMap<>();
-    for (final Map.Entry<String, ClassData> entry
-        : receivedFileDataClassData.entrySet()) {
-      final ClassData2 cd = new ClassData2(); // NOPMD
-
-      switch (entry.getValue().getType()) {
-        case INTERFACE:
-          cd.setType(ClassType2.INTERFACE);
-          break;
-        case ABSTRACT_CLASS:
-          cd.setType(ClassType2.ABSTRACT_CLASS);
-          break;
-        case CLASS:
-          cd.setType(ClassType2.CLASS);
-          break;
-        case ENUM:
-          cd.setType(ClassType2.ENUM);
-          break;
-        case ANONYMOUS_CLASS:
-          cd.setType(ClassType2.ANONYMOUS_CLASS);
-          break;
-        default:
-          // nothing to do
-      }
-      cd.setModifier(entry.getValue().getModifierList());
-      cd.setIntrfc(entry.getValue().getInterfaceList());
-
-      final List<FieldData2> field = new ArrayList<>(); // NOPMD
-      for (final FieldData fd : entry.getValue().getFieldList()) {
-        final FieldData2 fd2 = new FieldData2(); // NOPMD
-        fd2.setName(fd.getName());
-        fd2.setType(fd.getType());
-        fd2.setModifier(fd.getModifierList());
-        field.add(fd2);
-      }
-      cd.setField(field);
-
-      cd.setInnerClass(entry.getValue().getInnerClassList());
-
-      final List<MethodData2> constructor = new ArrayList<>(); // NOPMD
-      for (final MethodData md : entry.getValue().getConstructorList()) {
-        final MethodData2 md2 = new MethodData2(); // NOPMD
-        md2.setReturnType(md.getReturnType());
-        md2.setModifier(md.getModifierList());
-        final List<ParameterData2> parameter = new ArrayList<>(); // NOPMD
-        for (final ParameterData pd : md.getParameterList()) {
-          final ParameterData2 pd2 = new ParameterData2(); // NOPMD
-          pd2.setName(pd.getName());
-          pd2.setType(pd.getType());
-          pd2.setModifier(pd.getModifierList());
-          parameter.add(pd2);
-        }
-        md2.setParameter(parameter);
-        md2.setOutgoingMethodCalls(md.getOutgoingMethodCallsList());
-        md2.setConstructor(md.getIsConstructor());
-        md2.setAnnotation(md.getAnnotationList());
-        md2.setMetric(md.getMetricMap());
-        constructor.add(md2);
-      }
-      cd.setConstructor(constructor);
-
-      final Map<String, MethodData2> methodData =
-          new HashMap<>(); // NOPMD
-
-      for (final Map.Entry<String, MethodData> entry2 : entry.getValue().getMethodDataMap()
-          .entrySet()) {
-        final MethodData2 md = new MethodData2(); // NOPMD
-        md.setReturnType(entry2.getValue().getReturnType());
-        md.setModifier(entry2.getValue().getModifierList());
-
-        final List<ParameterData2> parameter = new ArrayList<>(); // NOPMD
-        for (final ParameterData pd : entry2.getValue().getParameterList()) {
-          final ParameterData2 pd2 = new ParameterData2(); // NOPMD
-          pd2.setName(pd.getName());
-          pd2.setType(pd.getType());
-          pd2.setModifier(pd.getModifierList());
-          parameter.add(pd2);
-        }
-        md.setParameter(parameter);
-
-        md.setOutgoingMethodCalls(entry2.getValue().getOutgoingMethodCallsList());
-        md.setConstructor(entry2.getValue().getIsConstructor());
-        md.setAnnotation(entry2.getValue().getAnnotationList());
-        md.setMetric(entry2.getValue().getMetricMap());
-        methodData.put(entry2.getKey(), md);
-      }
-      cd.setMethodData(methodData);
-      cd.setVariable(entry.getValue().getVariableList());
-      cd.setSuperClass(entry.getValue().getSuperClass());
-      cd.setEnumConstant(entry.getValue().getEnumConstantList());
-      cd.setAnnotation(entry.getValue().getAnnotationList());
-      cd.setClassMetric(entry.getValue().getMetricMap());
-
-      classData.put(entry.getKey(), cd);
+    for (Map.Entry<String, ClassData> entry : receivedFileDataClassData.entrySet()) {
+      classData.put(entry.getKey(), transformClassData(entry.getValue()));
     }
     fileReport.setClassData(classData);
+  }
 
-    fileReport.setFileMetric(receivedFileDataMetric);
-    fileReport.setAuthor(receivedFileDataAuthor);
-    fileReport.setModifiedLines(receivedFileDataModifiedLines);
-    fileReport.setAddedLines(receivedFileDataAddedLines);
-    fileReport.setDeletedLines(receivedFileDataDeletedLines);
+  private ClassData2 transformClassData(ClassData data) {
+    ClassData2 cd = new ClassData2();
+    switch (data.getType()) {
+      case INTERFACE:
+        cd.setType(ClassType2.INTERFACE);
+        break;
+      case ABSTRACT_CLASS:
+        cd.setType(ClassType2.ABSTRACT_CLASS);
+        break;
+      case CLASS:
+        cd.setType(ClassType2.CLASS);
+        break;
+      case ENUM:
+        cd.setType(ClassType2.ENUM);
+        break;
+      case ANONYMOUS_CLASS:
+        cd.setType(ClassType2.ANONYMOUS_CLASS);
+        break;
+      default:
+        // nothing to do
+    }
+    cd.setModifier(data.getModifierList());
+    cd.setIntrfc(data.getInterfaceList());
+    cd.setField(transformFields(data.getFieldList()));
+    cd.setInnerClass(data.getInnerClassList());
+    cd.setConstructor(transformMethods(data.getConstructorList()));
+    cd.setMethodData(transformMethodData(data.getMethodDataMap()));
+    cd.setVariable(data.getVariableList());
+    cd.setSuperClass(data.getSuperClass());
+    cd.setEnumConstant(data.getEnumConstantList());
+    cd.setAnnotation(data.getAnnotationList());
+    cd.setClassMetric(data.getMetricMap());
+    return cd;
+  }
 
-    // a filereport for the same file in the same commit can be received more than one time
-    // Thus, we make sure to update it if it existed before
-    final FileReport oldReport = FileReport
+  private List<FieldData2> transformFields(List<FieldData> fields) {
+    List<FieldData2> transformedFields = new ArrayList<>();
+    for (FieldData fd : fields) {
+      FieldData2 fd2 = new FieldData2();
+      fd2.setName(fd.getName());
+      fd2.setType(fd.getType());
+      fd2.setModifier(fd.getModifierList());
+      transformedFields.add(fd2);
+    }
+    return transformedFields;
+  }
+
+  private List<MethodData2> transformMethods(List<MethodData> methods) {
+    List<MethodData2> transformedMethods = new ArrayList<>();
+    for (MethodData md : methods) {
+      transformedMethods.add(transformMethod(md));
+    }
+    return transformedMethods;
+  }
+
+  private MethodData2 transformMethod(MethodData md) {
+    MethodData2 md2 = new MethodData2();
+    md2.setReturnType(md.getReturnType());
+    md2.setModifier(md.getModifierList());
+    md2.setParameter(transformParameters(md.getParameterList()));
+    md2.setOutgoingMethodCalls(md.getOutgoingMethodCallsList());
+    md2.setConstructor(md.getIsConstructor());
+    md2.setAnnotation(md.getAnnotationList());
+    md2.setMetric(md.getMetricMap());
+    return md2;
+  }
+
+  private List<ParameterData2> transformParameters(List<ParameterData> parameters) {
+    List<ParameterData2> transformedParameters = new ArrayList<>();
+    for (ParameterData pd : parameters) {
+      ParameterData2 pd2 = new ParameterData2();
+      pd2.setName(pd.getName());
+      pd2.setType(pd.getType());
+      pd2.setModifier(pd.getModifierList());
+      transformedParameters.add(pd2);
+    }
+    return transformedParameters;
+  }
+
+  private Map<String, MethodData2> transformMethodData(Map<String, MethodData> methodDataMap) {
+    Map<String, MethodData2> methodData = new HashMap<>();
+    for (Map.Entry<String, MethodData> entry : methodDataMap.entrySet()) {
+      methodData.put(entry.getKey(), transformMethod(entry.getValue()));
+    }
+    return methodData;
+  }
+
+  private void updateFileReport(FileData fileData, FileReport fileReport) {
+    final String fqFileName = fileData.getPackageName() + "." + fileData.getFileName();
+    final String commitId = fileData.getCommitID();
+    final String token = fileData.getLandscapeToken();
+    final String appName = fileData.getApplicationName();
+    final String packageName = fileData.getPackageName();
+
+    FileReport oldReport = FileReport
         .findByTokenAndAppNameAndPackageNameAndFileNameAndCommitId(
-            receivedFileDataLandscapeToken, receivedFileDataAppName,
-            receivedFileDataPackageName + "." + receivedFileDataFileName,
-            receivedFileDataCommitId);
+            token, appName, packageName + "." + fqFileName, commitId);
 
-    if (oldReport != null) { // NOPMD
-      oldReport.setPackageName(fileReport.getPackageName());
-      oldReport.setImportName(fileReport.getImportName());
-      oldReport.setClassData(fileReport.getClassData());
-      oldReport.setFileMetric(fileReport.getFileMetric());
-      oldReport.setAuthor(fileReport.getAuthor());
-      oldReport.setModifiedLines(fileReport.getModifiedLines());
-      oldReport.setAddedLines(fileReport.getAddedLines());
-      oldReport.setDeletedLines(fileReport.getDeletedLines());
-      oldReport.update();
+    if (oldReport != null) {
+      updateExistingFileReport(oldReport, fileReport);
     } else {
       fileReport.persist();
     }
   }
 
+  private void updateExistingFileReport(FileReport oldReport, FileReport newReport) {
+    oldReport.setPackageName(newReport.getPackageName());
+    oldReport.setImportName(newReport.getImportName());
+    oldReport.setClassData(newReport.getClassData());
+    oldReport.setFileMetric(newReport.getFileMetric());
+    oldReport.setAuthor(newReport.getAuthor());
+    oldReport.setModifiedLines(newReport.getModifiedLines());
+    oldReport.setAddedLines(newReport.getAddedLines());
+    oldReport.setDeletedLines(newReport.getDeletedLines());
+    oldReport.update();
+  }
 }
