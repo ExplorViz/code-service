77af5de7b629aeebb535406274b9ea51aa7339fb
Alexander Krause <akr@informatik.uni-kiel.de>
2024-08-13 10:38:17 +0200

    records instead of POJOs for commit-tree dtos

--- src/main/java/net/explorviz/code/dto/commit/tree/BranchDto.java
+++ src/main/java/net/explorviz/code/dto/commit/tree/BranchDto.java
@@ -2,41 +2,6 @@ package net.explorviz.code.dto.commit.tree;
 
 import java.util.List;
 
-public class BranchDto {
-
-  private String name;
-  private List<String> commits;
-  private BranchPointDto branchPoint;
-
-  public BranchDto(final String name, final List<String> commits,
-      final BranchPointDto branchPoint) {
-    this.name = name;
-    this.commits = commits;
-    this.branchPoint = branchPoint;
-  }
-
-  public String getName() {
-    return this.name;
-  }
-
-  public void setName(final String name) {
-    this.name = name;
-  }
-
-  public List<String> getCommits() {
-    return this.commits;
-  }
-
-  public void setCommits(final List<String> commits) {
-    this.commits = commits;
-  }
-
-  public BranchPointDto getBranchPoint() {
-    return this.branchPoint;
-  }
-
-  public void setBranchPoint(final BranchPointDto branchPoint) {
-    this.branchPoint = branchPoint;
-  }
+public record BranchDto(String name, List<String> commits, BranchPointDto branchPoint) {
 
 }
--- src/main/java/net/explorviz/code/dto/commit/tree/BranchPointDto.java
+++ src/main/java/net/explorviz/code/dto/commit/tree/BranchPointDto.java
@@ -1,30 +1,6 @@
 package net.explorviz.code.dto.commit.tree;
 
-public class BranchPointDto {
-
-  private String name;
-  private String commit;
-
-  public BranchPointDto(final String name, final String commit) {
-    this.name = name;
-    this.commit = commit;
-  }
-
-  public String getName() {
-    return this.name;
-  }
-
-  public void setName(final String name) {
-    this.name = name;
-  }
-
-  public String getCommit() {
-    return this.commit;
-  }
-
-  public void setCommit(final String commit) {
-    this.commit = commit;
-  }
-
+public record BranchPointDto(String name, String commit) {
 
 }
+
